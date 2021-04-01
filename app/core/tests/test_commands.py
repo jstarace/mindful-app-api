ebec87ab6f292dda2d09212e2f2d9d92356e6b55
Jason <jstarace@mac.com>
2021-04-01 02:48:25 -0700

    fixed

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
-from django.test import test_create_new_superuser
+from django.test import TestCase
 
 
 class CommandTests(TestCase):
