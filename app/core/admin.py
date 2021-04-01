8d6020101d4ff19345bdeee02a6ed52577e6aacf
Jason <jstarace@mac.com>
2021-03-31 22:54:17 -0700

    Travis cleaning

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 
+
 from core import models
 
 
@@ -9,11 +10,11 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-        (None, {'fields':('email', 'password')}),
+        (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('name', )}),
         (
             _('Permissions'),
-            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+            {'fields':('is_active', 'is_staff', 'is_superuser')}
         ),
         (_('Important dates'), {'fields': ('last_login',)})
     )
